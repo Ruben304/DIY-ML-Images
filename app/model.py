f1659aa0ed18428146cb804138feff6dfc0343d1
Ruben304 <ruben.carbajal6502@gmail.com>
2024-02-16 14:28:32 -0500

    added dataset model and its routes

--- app/model.py
+++ app/model.py
@@ -9,11 +9,18 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True) # creates a column for ids, and marked as primary key to identify row
     name = db.Column(db.String(128), nullable=False) # creates a column for names, stores up to 128 char and cant be empty
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) # creates a column for user_id to linnk project to users, and project must be associated with users
-    status = db.Column(db.String(128), nullable=False) # creates a column for the status of project, andn cant be empty
-    project_type = db.Column(db.String(128), nullable=False) # creates a column of project being image or object
+    status = db.Column(db.String(128), nullable=False) # creates a column for the status of project, and cant be empty
+    datasets = db.relationship('Dataset', backref='user', lazy=True) # create association of project to a dataset
 
 # similar to project 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True) # user id
     username = db.Column(db.String(128), unique=True, nullable=False) # username and ensures uniqueness
     projects = db.relationship('Project', backref='user', lazy=True) # creates association of User and Projects, and lazy=True allows projects association to be lister
+
+class Dataset(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # user id
+    data_name = db.Column(db.String(128), nullable=False) # name
+    data_pupose = db.Column(db.String(128), nullable=False) # training or inference
+    data_type = db.Column(db.String(128), nullable=False)  # image classification or object detection
+    # add actual metadata
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from app import app, db
-from app.models import Project, User
+from app.models import Project, User, Dataset
 
 # POST = create
 # GET = retrieve
@@ -13,7 +13,7 @@ from app.models import Project, User
 def create_project():
     data = request.json
 
-    new_project = Project(name= data['name'], user_id= data['user_id'], status= ['status'], project_type= ['project_type'])
+    new_project = Project(name= data['name'], user_id= data['user_id'], status= ['status'], datasets=['datasets'])
 
     # add user to the db session and commit
     db.session.add(new_project)
@@ -58,7 +58,7 @@ def get_project(project_id):
         'name': project.name,
         'user_id': project.user_id,
         'status': project.status,
-        'project_type': project.project_type
+        'datasets': project.datasets
     }
 
     return jsonify(project_data), 200
@@ -72,7 +72,7 @@ def list_projects():
         'name': project.name,
         'user_id': project.user_id,
         'status': project.status,
-        'project_type': project.project_type
+        'datasets': project.datasets
     } for project in projects]  # create a list of the details
 
     return jsonify(projects_data), 200
@@ -86,14 +86,14 @@ def list_user_projects(current_user):
         'name': project.name,
         'user_id': project.user_id,
         'status': project.status,
-        'project_type': project.project_type
+        'datasets': project.datasets
     } for project in projects]  # create list of project details
 
     return jsonify(projects_data), 200
 
 # get all users info 
 @app.route('/users', methods=['GET'])
-def list_projects():
+def list_users():
     users = User.query.all()  # fetch all users instances in db
     users_data = [{
         'id': user.id,
@@ -118,3 +118,74 @@ def get_user(user_id):
     }
 
     return jsonify(user_info), 200
+
+# deleting a project == need to figure out if deleting datasets associated to project
+@app.route('/project/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+    project = Project.query.get(project_id)
+
+    # check if exist
+    if not project:
+        return jsonify({'message': 'Project not found'}), 404
+
+    db.session.delete(project)
+    db.session.commit()
+
+    return jsonify({'message': 'Project deleted successfully'}), 200
+
+# deleting a user == need to figure out if deleting projects associated to user
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = Project.query.get(user_id)
+
+    # check if exist
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({'message': 'User deleted successfully'}), 200
+
+# get all datasets info 
+@app.route('/datasets', methods=['GET'])
+def list_datasets():
+    datasets = Dataset.query.all()  # fetch all datasets instances in db
+    datasets_data = [{
+        'id': dataset.id,
+        'data_name': dataset.data_name,
+        'data_purpose': dataset.data_purpose,
+        'data_type': dataset.data_type
+    } for dataset in datasets]  # create a list of the details
+
+    return jsonify(datasets_data), 200
+
+# get info of specific dataset 
+@app.route('/datasets/<int:dataset_id>', methods=['GET'])
+def get_dataset(dataset_id):
+    dataset = Dataset.query.get(dataset_id)
+    
+    if not dataset:
+        return jsonify({'message': 'Dataset not found'}), 404
+    
+    dataset_info = {
+        'id': dataset.id,
+        'data_name': dataset.data_name,
+        'data_purpose': dataset.data_purpose,
+        'data_type': dataset.data_type
+    }
+
+    return jsonify(dataset_info), 200
+
+@app.route('/dataset/<int:user_id>', methods=['DELETE'])
+def delete_datset(dataset_id):
+    dataset = Project.query.get(dataset_id)
+
+    # check if exist
+    if not dataset:
+        return jsonify({'message': 'Dataset not found'}), 404
+
+    db.session.delete(dataset)
+    db.session.commit()
+
+    return jsonify({'message': 'Dataset deleted successfully'}), 200
